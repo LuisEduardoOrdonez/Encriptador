56002ed8f0762063f684d2a85e988cf7b3a0a6a8
Luis Eduardo Ordonez <ordonezmoralesluis@gmail.com>
2024-03-18 12:59:43 -0500

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+function validarTexto() {
+    let texto = document.getElementById("texto").value;
+    let expresion = /^[a-z\s]+$/;
+
+      if (!expresion.test(texto)) {
+        alert  ("Por favor, ingresa solo letras minúsculas y sin acento.");
+        document.getElementById("texto").value = "";
+      
+    } 
+}
+
+function encriptar(){
+    let texto = document.getElementById( "texto" ).value;
+    let textoEncrip = document.getElementById("texto-Encrip");
+    let comentario  = document.getElementById('comentario');
+
+    let textoCifradado = texto
+                         
+                         .replace(/e/gi, "enter")
+                         .replace(/o/gi, "ober")
+                         .replace(/i/gi, "imes")
+                         .replace(/a/gi, "ai")
+                         .replace(/u/gi, "ufat")
+
+    if (texto.length !=0){
+        document.getElementById( "texto" ).value = textoCifradado;
+        document.getElementById("textoDesencriptado").value = textoCifradado;
+        textoEncrip.textContent = "Texto Encriptado Con Exito";
+        comentario.textContent ="";
+    }  else {
+        alert("Ingrese un Texto");
+   }                   
+}
+function desencriptar(){
+    let texto = document.getElementById( "texto" ).value;
+    let textoEncrip = document.getElementById("texto-Encrip");
+    let comentario  = document.getElementById('comentario');
+    //texto.tolowerCase();
+    //alert(texto);
+    let textoCifradado = texto
+                         .replace(/ufat/gi, "u")
+                         .replace(/ober/gi, "o")
+                         .replace(/imes/gi, "i")
+                         .replace(/enter/gi, "e")                              
+                         .replace(/ai/gi, "a")
+                         
+
+        if (texto.length !=0){
+            document.getElementById( "texto" ).value = textoCifradado;
+            document.getElementById("textoDesencriptado").value = textoCifradado
+            textoEncrip.textContent = "Texto Desencriptado Con Exito";
+            comentario.textContent="";
+        }  else {
+            alert('Ingrese un Texto')
+        }             
+}
+function copiar(){
+    let valor = document.getElementById('textoDesencriptado');
+    console.log(valor)
+        if (valor.value != 0){
+        valor.select();
+        navigator.clipboard.writeText(valor.value);
+        alert("Texto Copiado Con Exito");
+    } else{
+        alert("No hay Texto a Copiar");
+  }
+    
+}
